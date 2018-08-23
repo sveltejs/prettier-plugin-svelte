aa958986bf58a81d894865ac38b156b05221a44a
James Birtles <jameshbirtles@gmail.com>
2018-08-23 20:22:05 +0100

    add some formatting specific tests

---
+++ test/formatting/index.ts
@@ -0,0 +1,25 @@
+import test from 'ava';
+import { readdirSync, readFileSync } from 'fs';
+import { format } from 'prettier';
+
+const dirs = readdirSync('test/formatting/samples');
+
+for (const dir of dirs) {
+    const input = readFileSync(`test/formatting/samples/${dir}/input.html`, 'utf-8').replace(
+        /\r?\n/g,
+        '\n',
+    );
+    const expectedOutput = readFileSync(
+        `test/formatting/samples/${dir}/output.html`,
+        'utf-8',
+    ).replace(/\r?\n/g, '\n');
+
+    test(`formatting: ${dir}`, t => {
+        const actualOutput = format(input, {
+            parser: 'svelte' as any,
+            plugins: [require.resolve('../../src')],
+            tabWidth: 4,
+        });
+        t.is(actualOutput, expectedOutput);
+    });
+}
