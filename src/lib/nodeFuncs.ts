80e39634e16ea2e05d368f11d08b363adb8afc7d
Clemens Akens <clebert@me.com>
2019-05-04 23:05:07 +0200

    drop support for v2, improve support for v3

--- src/lib/nodeFuncs.ts
+++
@@ -1,5 +0,0 @@
-import { Node, BindingNodeV2 } from '../print/nodes';
-
-export function isBindingNodeV2(node: Node): node is BindingNodeV2 {
-    return node.type === 'Binding' && 'value' in node;
-}
