adb6c935fb0c9ef9df447d1b7d96c4734636ac0a
Jesse Skinner <jesse@thefutureoftheweb.com>
2020-08-18 16:08:15 -0400

    add curly braces when attribute shorthand is used but not allowed, fixes #110

--- src/print/index.ts
+++ src/print/index.ts
@@ -229,18 +229,21 @@ export function print(path: FastPath, options: ParserOptions, print: PrintFn): D
 
             if (isAttributeShorthand && options.svelteAllowShorthand) {
                 return concat([line, '{', node.name, '}']);
-						} else {
-							const def: Doc[] = [line, node.name];
-							if (node.value !== true) {
-									def.push('=');
-									const quotes = !hasLoneMustacheTag || options.svelteStrictMode;
+			} else {
+                const def: Doc[] = [line, node.name];
+                if (node.value !== true) {
+                    def.push('=');
+                    const quotes = !hasLoneMustacheTag || options.svelteStrictMode;
+                    const curlies = quotes && isAttributeShorthand && !options.svelteAllowShorthand;
 
-									quotes && def.push('"');
-									def.push(...path.map(childPath => childPath.call(print), 'value'));
-									quotes && def.push('"');
-							}
-							return concat(def);
-						}
+                    quotes && def.push('"');
+                    curlies && def.push('{');
+                    def.push(...path.map(childPath => childPath.call(print), 'value'));
+                    curlies && def.push('}');
+                    quotes && def.push('"');
+                }
+                return concat(def);
+            }
         }
         case 'MustacheTag':
             return concat(['{', printJS(path, print, 'expression'), '}']);
