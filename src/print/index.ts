36ff82cb815c0906ae7bafbcf45b2de56cde1d98
Jesse Skinner <jesse@thefutureoftheweb.com>
2020-08-30 06:01:24 -0400

    Add curly braces when attribute shorthand is used but not allowed (#119)
    + Some refactorings to make code more clear
    Closes #110

--- src/print/index.ts
+++ src/print/index.ts
@@ -1,5 +1,5 @@
 import { FastPath, Doc, doc, ParserOptions } from 'prettier';
-import { Node, MustacheTagNode, IfBlockNode } from './nodes';
+import { Node, IfBlockNode, AttributeNode } from './nodes';
 import { isASTNode, isPreTagContent } from './helpers';
 import { extractAttributes } from '../lib/extractAttributes';
 import { getText } from '../lib/getText';
@@ -12,6 +12,8 @@ import {
     isInlineElement,
     isInlineNode,
     isEmptyNode,
+    isLoneMustacheTag,
+    isOrCanBeConvertedToShorthand,
 } from './node-helpers';
 import {
     isLine,
@@ -224,43 +226,26 @@ export function print(path: FastPath, options: ParserOptions, print: PrintFn): D
             return node.expression.name;
         }
         case 'Attribute': {
-            const hasLoneMustacheTag =
-                node.value !== true &&
-                node.value.length === 1 &&
-                node.value[0].type === 'MustacheTag';
-            let isAttributeShorthand =
-                node.value !== true &&
-                node.value.length === 1 &&
-                node.value[0].type === 'AttributeShorthand';
-
-            // Convert a={a} into {a}
-            if (hasLoneMustacheTag) {
-                const expression = (node.value as [MustacheTagNode])[0].expression;
-                isAttributeShorthand =
-                    expression.type === 'Identifier' && expression.name === node.name;
-            }
-
-            if (isAttributeShorthand && options.svelteAllowShorthand) {
-                return concat([line, '{', node.name, '}']);
-            } else {
-                const def: Doc[] = [line, node.name];
-                if (node.value !== true) {
-                    def.push('=');
-                    const quotes = !hasLoneMustacheTag || options.svelteStrictMode;
-
-                    quotes && def.push('"');
-
-                    const valueDocs = path.map((childPath) => childPath.call(print), 'value');
-
-                    if (!quotes || !formattableAttributes.includes(node.name)) {
-                        def.push(concat(valueDocs));
-                    } else {
-                        def.push(indent(group(concat(trim(valueDocs, isLine)))));
-                    }
-
-                    quotes && def.push('"');
+            if (isOrCanBeConvertedToShorthand(node)) {
+                if (options.svelteStrictMode) {
+                    return concat([line, node.name, '="{', node.name, '}"']);
+                } else if (options.svelteAllowShorthand) {
+                    return concat([line, '{', node.name, '}']);
+                } else {
+                    return concat([line, node.name, '={', node.name, '}']);
                 }
-                return concat(def);
+            } else {
+                if (node.value === true) {
+                    return concat([line, node.name]);
+                }
+
+                const quotes = !isLoneMustacheTag(node.value) || options.svelteStrictMode;
+                const attrNodeValue = printAttributeNodeValue(path, print, quotes, node);
+                if (quotes) {
+                    return concat([line, node.name, '=', '"', attrNodeValue, '"']);
+                } else {
+                    return concat([line, node.name, '=', attrNodeValue]);
+                }
             }
         }
         case 'MustacheTag':
@@ -486,6 +471,21 @@ export function print(path: FastPath, options: ParserOptions, print: PrintFn): D
 
     console.error(JSON.stringify(node, null, 4));
     throw new Error('unknown node type: ' + node.type);
+}
+
+function printAttributeNodeValue(
+    path: FastPath<any>,
+    print: PrintFn,
+    quotes: boolean,
+    node: AttributeNode,
+) {
+    const valueDocs = path.map((childPath) => childPath.call(print), 'value');
+
+    if (!quotes || !formattableAttributes.includes(node.name)) {
+        return concat(valueDocs);
+    } else {
+        return indent(group(concat(trim(valueDocs, isLine))));
+    }
 }
 
 function printChildren(path: FastPath, print: PrintFn): Doc[] {
