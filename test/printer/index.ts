a0b0ba1226c8161948cd6d8b559a9341e55c0418
Simon Holthausen <simon.holthausen@accso.de>
2020-09-26 14:05:52 +0200

    add support for running only dedicated tests

--- test/printer/index.ts
+++ test/printer/index.ts
@@ -2,13 +2,20 @@ import test from 'ava';
 import { readdirSync, readFileSync, existsSync } from 'fs';
 import { format } from 'prettier';
 
-const files = readdirSync('test/printer/samples').filter(name => name.endsWith('.html'));
+let files = readdirSync('test/printer/samples').filter((name) => name.endsWith('.html'));
+const endsWithOnly = (f: string): boolean => f.endsWith('.only.html');
+const hasOnly = files.some(endsWithOnly);
+files = !hasOnly ? files : files.filter(endsWithOnly);
+
+if (process.env.CI && hasOnly) {
+    throw new Error('.only tests present');
+}
 
 for (const file of files) {
     const input = readFileSync(`test/printer/samples/${file}`, 'utf-8').replace(/\r?\n/g, '\n');
     const options = readOptions(`test/printer/samples/${file.replace('.html', '.options.json')}`);
 
-    test(`printer: ${file.slice(0, file.length - '.html'.length)}`, t => {
+    test(`printer: ${file.slice(0, file.length - '.html'.length)}`, (t) => {
         const actualOutput = format(input, {
             parser: 'svelte' as any,
             plugins: [require.resolve('../../src')],
