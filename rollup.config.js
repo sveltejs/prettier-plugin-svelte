1201870212ee571efb2e5dc0de6468a92ddb8a19
Simon Holthausen <simon.holthausen@accso.de>
2021-01-05 18:02:46 +0100

    Bump dependencies
    Not bumping the TS plugin because latest version does not build the output correctly and I don't know how to fix it

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,10 @@
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 import typescript from 'rollup-plugin-typescript';
 
 export default {
     input: 'src/index.ts',
-    plugins: [resolve(), commonjs(), typescript({ typescript: require('typescript') })],
+    plugins: [resolve(), commonjs(), typescript()],
     external: ['prettier', 'svelte'],
     output: {
         file: 'plugin.js',
