911b655cddbceb2e621bdc339fbed5304793da63
Simon H <5968653+dummdidumm@users.noreply.github.com>
2021-02-03 15:18:56 +0100

    (fix) case sensitive script/style snipping (#200)
    Fixes #198

--- src/lib/snipTagContent.ts
+++ src/lib/snipTagContent.ts
@@ -1,7 +1,7 @@
 export const snippedTagContentAttribute = '✂prettier:content✂';
 
 export function snipTagContent(tagName: string, source: string, placeholder = ''): string {
-    const regex = new RegExp(`<${tagName}([^]*?)>([^]*?)<\/${tagName}>`, 'gi');
+    const regex = new RegExp(`<${tagName}([^]*?)>([^]*?)<\/${tagName}>`, 'g');
     return source.replace(regex, (_, attributes, content) => {
         const encodedContent = Buffer.from(content).toString('base64');
         return `<${tagName}${attributes} ${snippedTagContentAttribute}="${encodedContent}">${placeholder}</${tagName}>`;
