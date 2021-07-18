8cd0f659d8861d1649d4d6c5b7d421ac8b22ef21
Christopher Fraser <chris@otterdev.io>
2021-07-19 01:49:13 +0800

    Split out cjs and esm configs

---
+++ rollup.standalone.esm.config.js
@@ -0,0 +1,31 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from 'rollup-plugin-typescript';
+import inject from '@rollup/plugin-inject';
+import alias from '@rollup/plugin-alias';
+import path from 'path';
+
+const srcDir = path.resolve(__dirname, 'src');
+export default {
+    input: 'src/index.ts',
+    plugins: [
+        alias({
+            entries: [{ find: /^prettier$/gm, replacement: `${srcDir}/standalone-shim` }],
+        }),
+        resolve({
+            preferBuiltins: false,
+            browser: true,
+        }),
+        commonjs(),
+        typescript({}),
+        inject({
+            Buffer: ['buffer', 'Buffer'],
+        }),
+    ],
+    external: ['prettier/esm/standalone', 'svelte'],
+    output: {
+        file: 'standalone/index.mjs',
+        format: 'esm',
+        sourcemap: true,
+    },
+};
