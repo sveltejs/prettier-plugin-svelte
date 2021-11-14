804b8915096cd5bb420b84b081dc4ceb604f9c73
Simon Holthausen <simon.holthausen@accso.de>
2021-11-14 15:36:38 +0100

    doc

--- src/standalone-shim.mjs
+++ src/standalone-shim.mjs
@@ -1,6 +1,6 @@
 import prettier from 'prettier/esm/standalone';
 export const doc = prettier.doc;
-// this file is used in rollup.standalone.esm.config.js
+// this file is used for the Rollup ESM standalone config
 // It replaces the prettier import
 // It is needed because the standalone versions do have default exports while
 // the non-standalone versions do not.
