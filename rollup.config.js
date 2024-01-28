fdb213f57c797d0996ef417d63ee5580749ea5e5
Curran <curran.kelleher@gmail.com>
2024-01-28 07:45:48 -0500

    Add browser build. Closes #69

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,43 @@
+import alias from '@rollup/plugin-alias';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from 'rollup-plugin-typescript';
 
-export default {
-    input: 'src/index.ts',
-    plugins: [resolve(), commonjs(), typescript()],
-    external: ['prettier', 'svelte'],
-    output: {
-        file: 'plugin.js',
-        format: 'cjs',
-        sourcemap: true,
+export default [
+    // CommonJS build
+    {
+        input: 'src/index.ts',
+        plugins: [resolve(), commonjs(), typescript()],
+        external: ['prettier', 'svelte/compiler'],
+        output: {
+            file: 'plugin.js',
+            format: 'cjs',
+            sourcemap: true,
+        },
     },
-};
+    // Browser build
+    // Supported use case: importing the plugin from a bundler like Vite or Webpack
+    // Unsupported use case: importing the plugin directly in the browser
+    {
+        input: 'src/index.ts',
+        plugins: [
+            alias({
+                // Replace imports from 'prettier' with 'prettier/standalone'
+                entries: [
+                    // But don't touch 'prettier/plugins/babel'
+                    { find: 'prettier/plugins/babel', replacement: 'prettier/plugins/babel' },
+                    { find: 'prettier', replacement: 'prettier/standalone' },
+                ],
+            }),
+            resolve(),
+            commonjs(),
+            typescript(),
+        ],
+        external: ['prettier/standalone', 'prettier/plugins/babel', 'svelte/compiler'],
+        output: {
+            file: 'browser.js',
+            format: 'esm',
+            sourcemap: true,
+        },
+    },
+];
