e1c6983f2fb1d7cdbf4cc490a48cc7df0896eecb
Curran <curran.kelleher@gmail.com>
2024-01-29 08:59:32 -0500

    Handle non-ASCII text in browser environments

--- src/base64-string.ts
+++ src/base64-string.ts
@@ -1,12 +1,20 @@
 // Base64 string encoding and decoding module.
 // Uses Buffer for Node.js and btoa/atob for browser environments.
+// We use TextEncoder/TextDecoder for browser environments because
+// they can handle non-ASCII characters, unlike btoa/atob.
 
 export const stringToBase64 =
     typeof Buffer !== 'undefined'
         ? (str: string) => Buffer.from(str).toString('base64')
-        : (str: string) => btoa(str);
+        : (str: string) =>
+              btoa(
+                  new TextEncoder()
+                      .encode(str)
+                      .reduce((acc, byte) => acc + String.fromCharCode(byte), ''),
+              );
 
 export const base64ToString =
     typeof Buffer !== 'undefined'
         ? (str: string) => Buffer.from(str, 'base64').toString()
-        : (str: string) => atob(str);
+        : (str: string) =>
+              new TextDecoder().decode(Uint8Array.from(atob(str), (c) => c.charCodeAt(0)));
